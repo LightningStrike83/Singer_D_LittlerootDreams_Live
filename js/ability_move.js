ab823d2f55d1064a2d168d9ff488ba8300e985f4
Dan Singer <d_singer@fanshaweonline.ca>
2025-08-08 20:34:07 -0400

    Bug Fixes
    -Fixed bug that displayed more results when no one learns a move
    -Fixed a bug that was making buttons on no results

--- js/ability_move.js
+++ js/ability_move.js
@@ -149,6 +149,30 @@ function calculatePokemon() {
                 const pokemonList = response.learned_by_pokemon;
 
                 let innerPromises = [];
+                
+                if (pokemonList.length === 0) {
+                    const resultsCon = document.querySelector("#results");
+                    const img = document.createElement("img")
+                    const p = document.createElement("p")
+                    const div = document.createElement("div")
+                    const button = document.querySelector("#show-sv-button")
+
+                    if (button) {
+                        button.remove()
+                    }
+
+                    resultsCon.innerHTML = ""
+
+                    img.src = "../images/pokemon_images/201qm.png"
+                    p.textContent = "Sorry, there are no Pokemon that match those results"
+                    div.setAttribute("id", "no-results-con")
+
+                    div.appendChild(img)
+                    div.appendChild(p)
+                    resultsCon.appendChild(div)
+
+                    return Promise.reject(new Error("No Pokémon found for move"));
+                }
 
                 pokemonList.forEach(pokemon => {
                     const url = pokemon.url;
@@ -160,6 +184,7 @@ function calculatePokemon() {
                         const innerP = fetch(`${baseURL}api-search/${number}`)
                         .then(response => response.json())
                         .then(function(response){
+
                         if (response.length > 0 && response[0].id !== undefined) {
                             results.move1Results.push(response[0].id);
                         } else {
@@ -184,6 +209,30 @@ function calculatePokemon() {
             .then(function(response){
                 const pokemonList = response.learned_by_pokemon;
                 let innerPromises = [];
+
+                if (pokemonList.length === 0) {
+                    const resultsCon = document.querySelector("#results");
+                    const img = document.createElement("img")
+                    const p = document.createElement("p")
+                    const div = document.createElement("div")
+                    const button = document.querySelector("#show-sv-button")
+
+                    if (button) {
+                        button.remove()
+                    }
+
+                    resultsCon.innerHTML = ""
+
+                    img.src = "../images/pokemon_images/201qm.png"
+                    p.textContent = "Sorry, there are no Pokemon that match those results"
+                    div.setAttribute("id", "no-results-con")
+
+                    div.appendChild(img)
+                    div.appendChild(p)
+                    resultsCon.appendChild(div)
+
+                    return Promise.reject(new Error("No Pokémon found for move"));
+                }
 
                 pokemonList.forEach(pokemon => {
                     const url = pokemon.url;
@@ -218,6 +267,30 @@ function calculatePokemon() {
                 const pokemonList = response.learned_by_pokemon;
                 let innerPromises = [];
 
+                if (pokemonList.length === 0) {
+                    const resultsCon = document.querySelector("#results");
+                    const img = document.createElement("img")
+                    const p = document.createElement("p")
+                    const div = document.createElement("div")
+                    const button = document.querySelector("#show-sv-button")
+
+                    if (button) {
+                        button.remove()
+                    }
+
+                    resultsCon.innerHTML = ""
+
+                    img.src = "../images/pokemon_images/201qm.png"
+                    p.textContent = "Sorry, there are no Pokemon that match those results"
+                    div.setAttribute("id", "no-results-con")
+
+                    div.appendChild(img)
+                    div.appendChild(p)
+                    resultsCon.appendChild(div)
+
+                    return Promise.reject(new Error("No Pokémon found for move"));
+                }
+
                 pokemonList.forEach(pokemon => {
                     const url = pokemon.url;
                     const match = url.match(/(?:^|\/)(\d+)(?:\/|$)/);
@@ -249,6 +322,30 @@ function calculatePokemon() {
             .then(function(response){
                 const pokemonList = response.learned_by_pokemon;
                 let innerPromises = [];
+
+                if (pokemonList.length === 0) {
+                    const resultsCon = document.querySelector("#results");
+                    const img = document.createElement("img")
+                    const p = document.createElement("p")
+                    const div = document.createElement("div")
+                    const button = document.querySelector("#show-sv-button")
+
+                    if (button) {
+                        button.remove()
+                    }
+
+                    resultsCon.innerHTML = ""
+
+                    img.src = "../images/pokemon_images/201qm.png"
+                    p.textContent = "Sorry, there are no Pokemon that match those results"
+                    div.setAttribute("id", "no-results-con")
+
+                    div.appendChild(img)
+                    div.appendChild(p)
+                    resultsCon.appendChild(div)
+
+                    return Promise.reject(new Error("No Pokémon found for move"));
+                }
 
                 pokemonList.forEach(pokemon => {
                     const url = pokemon.url;
@@ -312,6 +409,8 @@ function calculatePokemon() {
 function populateResults(finalresults, i = 0) {
     const resultsCon = document.querySelector("#results");
 
+    console.log("finalResults length:", finalresults.length)
+
     if (finalresults.length === 0) {
         const resultsCon = document.querySelector("#results");
         const img = document.createElement("img")
@@ -327,9 +426,21 @@ function populateResults(finalresults, i = 0) {
         div.appendChild(img)
         div.appendChild(p)
         resultsCon.appendChild(div)
+
+        return;
     }
 
-    if (i >= finalresults.length) return; 
+    if (i >= finalresults.length) {
+        const button = document.createElement("button")
+
+        button.innerText = "Show Scarlet and Violet Pokemon only?"
+        button.setAttribute("id", "show-sv-button")
+        button.addEventListener("click", deleteNonSV)
+
+        resultsCon.appendChild(button)
+
+        return;
+    }
 
     if (i === 0) {
         resultsCon.innerHTML = ""
@@ -343,14 +454,18 @@ function populateResults(finalresults, i = 0) {
             const img = document.createElement("img");
             const p = document.createElement("p");
             const div = document.createElement("div");
+            
 
             img.src = `../images/pokemon_images/${response[0].number}.png`;
             p.textContent = `${response[0].name}`;
             div.setAttribute("data-ScarVio", `${response[0].in_ScarVio}`);
+            div.setAttribute("class", "am-result")
+            
 
             div.appendChild(img);
             div.appendChild(p);
             resultsCon.appendChild(div);
+            
 
             populateResults(finalresults, i + 1);
         })
@@ -359,6 +474,27 @@ function populateResults(finalresults, i = 0) {
 
             populateResults(finalresults, i + 1);
         });
+
+        
+}
+
+function deleteNonSV() {
+    const pokeDivs = document.querySelectorAll(".am-result")
+    const button = document.querySelector("#show-sv-button")
+
+    pokeDivs.forEach(div => {
+        if (div.dataset.ScarVio !== "y") {
+            if (div.style.display === "none") {
+                div.style.display = "flex"
+
+                button.innerText = "Show Scarlet and Violet Pokemon only?"
+            } else {
+                div.style.display = "none"
+
+                button.innerText = "Show All Pokemon?"
+            }
+        }
+    })
 }
 
 svButton.addEventListener("click", svMoves)
