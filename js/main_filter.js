1ab33c3f448e0e8973dbf1d46398d5087021219a
Dan Singer <d_singer@fanshaweonline.ca>
2025-12-28 00:10:18 -0500

    Search Function (Desktop)
    -Added and enabled search function for desktop
    -Ensured searching coincides with filtering
    -Wrote new function to hide titles that's more reliable
    -Ensured capitalization and punctuation doesn't affect searches
    -Ensured filter and search will always be visible

--- js/main_filter.js
+++ js/main_filter.js
@@ -1,60 +1,262 @@
 const filter = document.querySelector("#cd-filter-select")
 const typeFilter = document.querySelector("#cd-type-select")
+const dexSearch = document.querySelector("#pokemon_search")
 
 function showSelection() {
     const sprites = document.querySelectorAll(".sprite_item")
-    const regionalText = document.querySelectorAll(".filter_text")
     const filterSelection = filter.value
     const typeFilterSelection = typeFilter.value
+    const text = dexSearch.value
+        .toLowerCase()
+        .replace(/[^a-z0-9]/gi, "")
+        .trim()
 
     sprites.forEach(sprite => {
-      let matchesRegion = true
-      let matchesType = true
+        let matchesRegion = true
+        let matchesType = true
+        let matchesText = true
 
-      if (filterSelection !== "" && filterSelection !== "all") {
-        matchesRegion = sprite.classList.contains(filterSelection)
-      }
+        const textSearch = sprite.querySelector(".pokemon_name")
+        if (!textSearch) return
 
-      if (typeFilterSelection !== "" && typeFilterSelection !== "all") {
-        matchesType =
-          sprite.dataset.type1 === typeFilterSelection ||
-          sprite.dataset.type2 === typeFilterSelection
-      }
+        const nameText = textSearch.textContent
+            .toLowerCase()
+            .replace(/[^a-z0-9]/gi, "")
 
-      if (matchesRegion && matchesType) {
-        sprite.style.display = ""
-      } else {
-        sprite.style.display = "none"
-      }
-      
+        if (filterSelection !== "" && filterSelection !== "all") {
+            matchesRegion = sprite.classList.contains(filterSelection)
+        }
+
+        if (typeFilterSelection !== "" && typeFilterSelection !== "all") {
+            matchesType =
+                sprite.dataset.type1 === typeFilterSelection ||
+                sprite.dataset.type2 === typeFilterSelection
+        }
+
+        if (!nameText.includes(text)) {
+            matchesText = false
+        }
+
+        if (matchesRegion && matchesType && matchesText) {
+            sprite.style.display = "flex"
+        } else {
+            sprite.style.display = "none"
+        }
     })
 
-    regionalText.forEach(text => {
-      let matchesRegion = true
-      let matchesType = true
+    checkRemaining()
+}
 
-      if (filterSelection !== "" && filterSelection !== "all") {
-        matchesRegion = text.classList.contains(`${filterSelection}_text`)
-      }
+function checkRemaining() {
+  const regionals = document.querySelectorAll(".regionals")
+  const megagmaxTitle =  document.querySelector("#mega_gmax")
+  const otherMainTitle = document.querySelector("#other")
 
-      if (typeFilterSelection !== "" && typeFilterSelection !== "all") {
-        matchesType = text.classList.contains(`${typeFilterSelection}_text`)
-      }
+  const alolanSprites = document.querySelectorAll(".regionals.alola")
+  const galarSprites = document.querySelectorAll(".regionals.galar")
+  const hisuiSprites = document.querySelectorAll(".regionals.hisui")
+  const paldeaSprites = document.querySelectorAll(".regionals.paldea")
+  const megaSprites = document.querySelectorAll(".megas")
+  const gmaxSprites = document.querySelectorAll(".gmaxes")
+  const otherSprites = document.querySelectorAll(".other_sprites")
+  const variantSprites = document.querySelectorAll(".cs_variants")
+  const pokestarSprites = document.querySelectorAll(".pokestars")
+  const betaSprites = document.querySelectorAll(".beta")
 
-      if (matchesRegion && matchesType) {
-        text.style.display = ""
-      } else {
-        text.style.display = "none"
-      }
+  const regionalTitle = document.querySelector("#regional")
+  const alolaTitle = document.querySelector("#alola_title")
+  const galarTitle = document.querySelector("#galar_title")
+  const hisuiTitle = document.querySelector("#hisui_title")
+  const paldeaTitle = document.querySelector("#paldea_title")
+  const megaTitle = document.querySelector("#mega_title")
+  const gmaxTitle = document.querySelector("#gmax_title")
+  const otherTitle = document.querySelector("#other_models")
+  const variantTitle = document.querySelector("#variant_title")
+  const pokestarTitle = document.querySelector("#ps_title")
+  const betaTitle = document.querySelector("#beta_title")
 
-      // if (!text.classList.contains(`${filterSelection}_text`)) {
-      //   text.style.display = "none"
-      // }
-    })
+  let r = 0
+  let a = 0
+  let g = 0
+  let h = 0
+  let p = 0
+  let m = 0
+  let gx = 0
+  let o = 0
+  let v = 0
+  let ps = 0
+  let b = 0
+
+  regionals.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      r++
+    }
+  })
+
+  alolanSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      a++
+    }
+  })
+
+  galarSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      g++
+    }
+  })
+
+  hisuiSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      h++
+    }
+  })
+
+  paldeaSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      p++
+    }
+  })
+
+  megaSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      m++
+    }
+  })
+
+  gmaxSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      gx++
+    }
+  })
+
+  otherSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      o++
+    }
+  })
+
+  variantSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      v++
+    }
+  })
+
+  pokestarSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      ps++
+    }
+  })
+
+  betaSprites.forEach(sprite => {
+    if (sprite.style.display === "flex") {
+      b++
+    }
+  })
+
+  if (r !== 0) {
+    regionalTitle.style.display = "block"
+    r = 0
+  } else {
+    regionalTitle.style.display = "none"
+    r = 0
+  }
+
+  if (a !== 0) {
+    alolaTitle.style.display = "block"
+    a = 0
+  } else {
+    alolaTitle.style.display = "none"
+    a = 0
+  }
+
+  if (g !== 0) {
+    galarTitle.style.display = "block"
+    g = 0
+  } else {
+    galarTitle.style.display = "none"
+    g = 0
+  }
+
+  if (h !== 0) {
+    hisuiTitle.style.display = "block"
+    h = 0
+  } else {
+    hisuiTitle.style.display = "none"
+    h = 0
+  }
+
+  if (p !== 0) {
+    paldeaTitle.style.display = "block"
+    p = 0
+  } else {
+    paldeaTitle.style.display = "none"
+    p = 0
+  }
+
+  if (m !== 0) {
+    megaTitle.style.display = "block"
+    m = 0
+  } else {
+    megaTitle.style.display = "none"
+    m = 0
+  }
+
+  if (gx !== 0) {
+    gmaxTitle.style.display = "block"
+    gx = 0
+  } else {
+    gmaxTitle.style.display = "none"
+    gx = 0
+  }
+
+  if (o !== 0) {
+    otherTitle.style.display = "block"
+    o = 0
+  } else {
+    otherTitle.style.display = "none"
+    o = 0
+  }
+
+  if (v !== 0) {
+    variantTitle.style.display = "block"
+    v = 0
+  } else {
+    variantTitle.style.display = "none"
+    v = 0
+  }
+
+  if (ps !== 0) {
+    pokestarTitle.style.display = "block"
+    ps = 0
+  } else {
+    pokestarTitle.style.display = "none"
+    ps = 0
+  }
+
+  if (b !== 0) {
+    betaTitle.style.display = "block"
+    b = 0
+  } else {
+    betaTitle.style.display = "none"
+    b = 0
+  }
+
+  if (megaTitle.style.display === "none" && gmaxTitle.style.display === "none") {
+    megagmaxTitle.style.display = "none"
+  } else {
+    megagmaxTitle.style.display = "block"
+  }
+
+  if (otherTitle.style.display === "none" && variantTitle.style.display === "none" && pokestarTitle.style.display === "none" && betaTitle.style.display === "none") {
+    otherMainTitle.style.display = "none"
+  } else {
+    otherMainTitle.style.display = "block"
+  }
 }
 
 filter.addEventListener("change", showSelection)
 typeFilter.addEventListener("change", showSelection)
+dexSearch.addEventListener("input", showSelection)
 
 // let all = document.querySelector("#all");
 // let kanto = document.querySelector("#kanto");
